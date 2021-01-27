2983a03c7876b416e4c2ca8422053b214412d4bc
Karolis Šarapnickis <pastas.k@gmail.com>
2021-01-27 22:56:37 +0200

    chore: add typescript playground

---
+++ playground/module-with-process/index.d.ts
@@ -0,0 +1,6 @@
+export type ModuleOutput = {
+  process_env: string;
+  MODE: string;
+};
+declare const _default: () => ModuleOutput;
+export default _default;
---
+++ playground/playground-typescript/src/renderer/index.ts
@@ -0,0 +1,24 @@
+import mwp, { ModuleOutput } from 'module-with-process';
+
+const log = (fn: () => ModuleOutput): void => {
+  console.log('mwp :>> ', fn);
+};
+
+log(mwp);
+
+document.getElementById('content').innerHTML = 'Edit the files and save to reload.';
+
+let count = 0;
+
+const render = () => {
+  document.getElementById(
+    'counter'
+  ).innerHTML = `Page has been open for <code>${count}</code> seconds.`;
+};
+
+render();
+
+setInterval(() => {
+  count += 1;
+  render();
+}, 1000);
