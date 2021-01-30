5a28a6d770244154e7361557687e1fdacef128d4
Karolis Šarapnickis <pastas.k@gmail.com>
2021-01-30 13:25:12 +0200

    chore: add Svelte + TypeScript example

---
+++ examples/svelte-typescript/README.md
@@ -0,0 +1,5 @@
+# Example: Svelte + Typescript
+
+## Configuration
+
+- [`@tsconfig/svelte`](https://www.npmjs.com/package/@tsconfig/svelte)
---
+++ examples/svelte-typescript/snowpack.config.js
@@ -0,0 +1,5 @@
+/** @type { import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  extends: 'electron-snowpack/config/snowpack.js',
+  plugins: ['@snowpack/plugin-svelte'],
+};
