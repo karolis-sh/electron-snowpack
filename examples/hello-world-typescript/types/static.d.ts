f41215291f8dbd9ba7c80a4ab1e41ab210b13ad3
Lauritz Hilsøe <mail+gh@lauritz.me>
2021-02-23 23:10:52 +0100

    Declare module for React example

--- examples/hello-world-typescript/types/static.d.ts
+++ examples/hello-world-typescript/types/static.d.ts
@@ -1,6 +1,10 @@
 /* Use this file to declare any custom file extensions for importing */
 /* Use this folder to also add/extend a package d.ts file, if needed. */
 
+declare module "electron-snowpack" {
+  const getAssetURL: (asset: string) => string;
+}
+
 /* CSS MODULES */
 declare module '*.module.css' {
   const classes: { [key: string]: string };
