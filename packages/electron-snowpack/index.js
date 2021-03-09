f9e2c8588b942d4d65b962c95268e5f052699ca8
Karolis Šarapnickis <pastas.k@gmail.com>
2021-03-09 19:20:40 +0200

    fix: refactor getAssetURL to not use deprecated url method

--- packages/electron-snowpack/index.js
+++ packages/electron-snowpack/index.js
@@ -1,12 +1,7 @@
-const url = require('url');
 const path = require('path');
 
 exports.getAssetURL = (asset) => {
   return process.env.NODE_ENV !== 'production'
     ? new URL(asset, `http://localhost:${process.env.SNOWPACK_PORT}/`).toString()
-    : url.format({
-        pathname: path.join(__dirname, asset),
-        protocol: 'file',
-        slashes: true,
-      });
+    : new URL(`file:///${path.join(__dirname, asset)}`).href;
 };
