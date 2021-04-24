ccc02c2996c33e32bc21d23dfba67d897d96daa4
Karolis Šarapnickis <pastas.k@gmail.com>
2021-04-24 14:26:59 +0300

    chore: add node module usage in playground

--- playground/playground/src/main/index.js
+++ playground/playground/src/main/index.js
@@ -6,7 +6,12 @@ import { ENTRY_FILE } from './constants';
 let mainWindow;
 
 function createMainWindow() {
-  const window = new BrowserWindow();
+  const window = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
   if (process.env.MODE !== 'production') {
     window.webContents.openDevTools();
